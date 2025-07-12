597865c24345cc03e6af1604c8c51651f90f8409
M. de Verteuil <mverteuil@users.noreply.github.com>
2025-07-11 21:29:06 -0400

    FEAT: Integrate Jinja2 templates and render index.html

--- src/web/main.py
+++ src/web/main.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="src/web/templates")
 
-@app.get("/")
-async def read_root():
-    return {"message": "BirdNET-Pi FastAPI is running!"}
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
